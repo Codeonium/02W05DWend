4a662368298cd3889334254be2a31f7523bc3cf2
codeonium <codeon2020@gmail.com>
2021-03-05 12:42:52 +0000

    added side note for use of Self in guest

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,8 +5,8 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("myself", 200)
+        self.dior = Guest("myself", 200) # the self is a sugar for Guest
 
     def test_guest_can_pay(self):
-        self.guest.pay_entry_fee(20)
-        self.assertEqual(180, self.guest.money)
+        self.dior.pay_entry_fee(20)
+        self.assertEqual(180, self.dior.money)
