fa4c1eeb5ecd19273d9fb706fe8d557ab01d3a34
codeonium <codeon2020@gmail.com>
2021-03-05 13:11:38 +0000

    first stop

--- classes/room.py
+++ classes/room.py
@@ -1,2 +1,8 @@
 class Room:
-    pass
+    def __init__(self, name, room_number, songs):
+        self.name = name
+        self.room_number = room_number
+        self.songs = songs
+    
+    def experience(self, guest, song, room):
+        pass
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,7 +1,20 @@
 import unittest
 
 from classes.room import Room
+from classes.guest import Guest
+from classes.song import Song
 
 
 class TestRoom(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.paco = Guest("Paco", 400)
+        despasito = Song("Despasito")
+        no_more = Song("no_more")
+
+        songs1 = [despasito, no_more]
+
+        self.room1 = Room("fun", 2, songs1)
+
+    def test_experience(self):
+        pass
