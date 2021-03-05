ce2b86812aed531ad0bf576857b914d77623937f
codeonium <codeon2020@gmail.com>
2021-03-05 16:36:10 +0000

    added a list of musics from 80's

--- classes/song.py
+++ classes/song.py
@@ -6,3 +6,28 @@ class Song:
         self.price = price
         self.duration = duration
     
+
+
+#   just a list of 80's:
+#   Bette Davis Eyes - Kim Carnes 
+#   Listen to Your Heart - Roxette
+#   Physical - Olivia Newton John
+#   Self Control - Laura Branigan
+#   Gloria - Laura Branigan
+#   Material Girl - Madonna
+#   Papa Don't Preach - Madonna
+#   Forever Young - Alphaville
+#   Everybody Wants to Rule the World - Tears for Fears
+#   Jump - Van Halen
+#   You Give Love a Bad Name - Bon Jovi
+#   You Spin Me Round (Like a Record) - Dead or Alive
+#   I'm Still Standing - Elton John
+#   When Doves Cry - Prince
+#   I'll Be There For You - Bon Jovi
+#   Please Don't Go - KC & The Sunshine Band
+#   The Time of My Life (I've Had) - Bill Medley & Jennifer Warnes
+#   How Am I Supposed to Live Without You - Michael Bolton
+#   That's What Friends Are For - Dionne Warwick & Friends
+#   Do That To Me One More Time - Captain & Tennille
+#   Can't Fight This Feeling - REO Speedwagon
+#   Careless Whisper - George Michael
